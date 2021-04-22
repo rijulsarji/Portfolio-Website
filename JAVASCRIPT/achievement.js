3fcc517f5ad168f918ae4bf80eea92488e55eb13
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2021-04-22 13:04:26 +0530

    Initial Commit

---
+++ JAVASCRIPT/achievement.js
@@ -0,0 +1,10 @@
+function oppen(el) {
+    var elem = document.getElementById(el);
+    elem.style.transition = "right 0.5s linear";
+    elem.style.right = "0px";
+}
+function closse(el) {
+    var elem = document.getElementById(el);
+    elem.style.transition = "right 0.5s linear";
+    elem.style.right = "-300px";
+}
